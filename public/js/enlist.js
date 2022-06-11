56fafaa96f716af8b4dcae7f629e8b195677c2eb
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2022-06-11 16:47:57 -0700

    Made non-working (at least on my browser) changes to how the enlist page works.

--- public/js/enlist.js
+++ public/js/enlist.js
@@ -1,8 +1,14 @@
-function openTab(tabName) {
-    let i, x;
-    x = document.getElementsByClassName("containerTab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
