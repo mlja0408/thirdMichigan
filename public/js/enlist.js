0d67b9bacf487747faad0da0c1a1f41d3807ba86
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2022-06-20 17:54:07 -0700

    Removed links to events page. Foumd a way to make enlist page work.

--- public/js/enlist.js
+++ public/js/enlist.js
@@ -1,14 +1,16 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+function expand() {
+    let coll = document.getElementsByClassName("collapsible");
+    let i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 }
