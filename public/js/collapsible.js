1079a36b861fba803360edea4c9de385e063ed84
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2023-02-16 12:11:15 -0800

    Updated some styling and attempted to change collapsibles...

---
+++ public/js/collapsible.js
@@ -0,0 +1,14 @@
+const coll = document.getElementsByClassName("collapsible");
+
+// Loop through all buttons and add a click event listener to toggle the content
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
---
+++ public/js/photos.js
@@ -0,0 +1,11 @@
+//const images = document.querySelectorAll("img");
+
+//images.forEach(image => {
+//    image.addEventListener("click", event => {
+//        console.log("Image was clicked!");
+//    });
+//});
+
+function image() {
+    alert('Image clicked.')
+}
