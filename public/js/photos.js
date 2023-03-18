d89b6e4890490b26d22c0ae722031ded029ee516
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2023-03-17 22:53:50 -0700

    Started adding gallery funcionality

--- public/js/photos.js
+++ public/js/photos.js
@@ -1,11 +1,34 @@
+let images = ' '
+let id = ' '
+let counter = 1; 
 window.addEventListener('DOMContentLoaded', function (event) {
-    const images = document.getElementsByClassName('galleryImage')
-
-    for (let i = 0; i < images.length; i++) {
-        images[i].addEventListener('click', handleClick);
+    images = document.getElementsByClassName('galleryImage')
+    try {
+		
+        for (let i = 0; i < images.length; i++) {
+            images[i].addEventListener('click', handleClick);
+            id = images[i].id= 'Image ' + counter;
+			counter++;
+        }
     }
+    catch (e) {
+        console.log(e)
+    }
+    
 });
 
-function handleClick() {
-    alert('Image Clicked')
+try {
+    function handleClick() {
+        console.log(this.id + ' Clicked' )
+		for (let i = 1; i <= counter; i++) {
+			const image = document.getElementById('Image ' + i);
+			// image.classList.remove('selected');
+		}
+		const clickedImage = document.getElementById(this.id);
+        clickedImage.classList.add('selected');
+
+    }
+}
+catch (e) {
+    console.log(e)
 }
