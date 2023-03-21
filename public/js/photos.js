f37f7852b168399fabe9ad8aa1893f1fb3efa7c9
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2023-03-20 19:56:47 -0700

    Created functioning gallery and updated minor styling

--- public/js/photos.js
+++ public/js/photos.js
@@ -19,15 +19,43 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
 try {
     function handleClick() {
-        console.log(this.id + ' Clicked' )
-		for (let i = 1; i <= counter; i++) {
-			const image = document.getElementById('Image ' + i);
-			// image.classList.remove('selected');
-		}
-		const clickedImage = document.getElementById(this.id);
-        clickedImage.classList.toggle('selected');
+        console.log(this.id + ' Clicked')
+        // Get the clicked image
+        let img = event.target;
 
+        // Create a new div to hold the enlarged image
+        let overlay = document.createElement("div");
+        overlay.style.position = "fixed";
+        overlay.style.top = "0";
+        overlay.style.left = "0";
+        overlay.style.width = "100%";
+        overlay.style.height = "100%";
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        overlay.style.display = "flex";
+        overlay.style.justifyContent = "center";
+        overlay.style.alignItems = "center";
+
+        // Create a new image that is a copy of the clicked image
+        let enlargedImg = new Image();
+        enlargedImg.src = img.src;
+        enlargedImg.style.maxWidth = "90%";
+        enlargedImg.style.maxHeight = "90%";
+        enlargedImg.style.width = "auto";
+        enlargedImg.style.height = "400px";
+        enlargedImg.style.objectFit = "contain";
+
+        // Add the enlarged image to the overlay div
+        overlay.appendChild(enlargedImg);
+
+        // Add the overlay div to the body of the page
+        document.body.appendChild(overlay);
+
+        // Add an event listener to the overlay div to remove it when clicked
+        overlay.addEventListener("click", function () {
+            document.body.removeChild(overlay);
+        });
     }
+
 }
 catch (e) {
     console.log(e)
